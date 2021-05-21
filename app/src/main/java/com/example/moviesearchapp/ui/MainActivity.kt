192739090093a740f315141e9d795c39fd72315b
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-05-21 20:09:02 +0300

    Create test layout file

--- app/src/main/java/com/example/moviesearchapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/moviesearchapp/ui/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.moviesearchapp.ui
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.moviesearchapp.R
 
 class MainActivity : AppCompatActivity() {
+    private var button: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button = findViewById(R.id.button)
+        button?.setOnClickListener {
+                Toast.makeText(
+                    this@MainActivity,
+                    button?.text.toString(),
+                    Toast.LENGTH_SHORT
+                ).show()
+        }
     }
 }
