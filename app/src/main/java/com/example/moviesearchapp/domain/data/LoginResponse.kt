adaf7911edd074c24f58022988fc9ca29ab75404
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-05-29 17:41:10 +0300

    Create data classes

---
+++ app/src/main/java/com/example/moviesearchapp/domain/data/LoginResponse.kt
@@ -0,0 +1,7 @@
+package com.example.moviesearchapp.domain.data
+
+data class LoginResponse(
+    val expires_at: String,
+    val request_token: String,
+    val success: Boolean
+)
---
+++ app/src/main/java/com/example/moviesearchapp/domain/data/TokenResponse.kt
@@ -0,0 +1,7 @@
+package com.example.moviesearchapp.domain.data
+
+data class TokenResponse(
+    val expires_at: String,
+    val request_token: String,
+    val success: Boolean
+)
