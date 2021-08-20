49a1b453ee0bce471307f2925709587e05968110
Moldobekov Mars <marsmoldobekov13144@gmail.com>
2021-08-20 12:59:47 +0300

    Make AppState be generic

--- app/src/main/java/com/example/moviesearchapp/viewmodel/AppState.kt
+++ app/src/main/java/com/example/moviesearchapp/viewmodel/AppState.kt
@@ -1,9 +1,7 @@
 package com.example.moviesearchapp.viewmodel
 
-import com.example.moviesearchapp.domain.data.Movie
-
-sealed class AppState {
-    data class Success(val movies: List<Movie>) : AppState()
-    data class Error(val error: Throwable) : AppState()
-    object Loading : AppState()
+sealed class AppState<T> {
+    data class Success<T>(val data: T) : AppState<T>()
+    data class Error(val error: Throwable) : AppState<Throwable>()
+    object Loading : AppState<Unit>()
 }
